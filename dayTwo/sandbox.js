76c7b7c06b7f4d5d97fa68d24e7f56d5127618fa
Patrick Attankurugu <patricka.azuma@gmail.com>
2022-06-30 05:59:15 -0700

    day two of thirty days of code

---
+++ dayTwo/sandbox.js
@@ -0,0 +1,48 @@
+//LOOPS
+
+//For Loops
+// for (let i = 0; i < 5; i++) {
+//   console.log("In Loop:", i);
+// }
+// console.log("Loop Finished");
+// const names = ["Young", "Man", "John", "Paul"];
+// for (let i = 0; i < names.length; i++) {
+// console.log(names[i]);
+//   let html = `<div>${names[i]}</div>`;
+//   console.log(html);
+// }
+
+// While Loop
+// let i = 0;
+// const names = ["Young", "Man", "John", "Paul"];
+// while (i < names.length) {
+//   console.log("I in Loop: ", names[i]);
+//   i++;
+// }
+
+//DO WHILE
+// let i = 1;
+// do {
+//   console.log("Val of I is: ", i);
+//   i++;
+// } while (i < 5);
+
+//IF STATEMENTS
+// const age = 20;
+// if (age > 20) {
+//   console.log("You are over twenty years old");
+// } else {
+//   console.log("You are not up to twenty years old");
+// }
+
+//LOGICAL OPERATORS OR || and AND &&
+// const password = "p@sskkkkkkkkkkkk";
+// if (password.length >= 12 && password.includes("@")) {
+//   console.log("That password is mighty strong");
+// } else if (password.length >= 8 || password.includes("@")) {
+//   console.log("That password is long enough");
+// } else {
+//   console.log("That is a very weak password");
+// }
+
+//BREAK AND CONTINUE
